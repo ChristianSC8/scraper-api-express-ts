4f1a44921c0191324e092586c819467cdd4addcf
Christian Giovani Supo Condori <136210953+ChristianSC8@users.noreply.github.com>
2025-03-23 20:22:19 -0500

    Delete build directory

--- build/config/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES = void 0;
-exports.ROLES = {
-    VIEWER: "viewer",
-    ADMIN: "admin",
-    EDITOR: "editor",
-};
--- build/helpers/exception_wrapper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wrapper = void 0;
-const wrapper = (fn) => (...args) => fn(...args).catch(args[2]);
-exports.wrapper = wrapper;
